a35d9ad9de9b1d1137fe67fd7f90be283e2804b1
Pravinraj Marimuthu <marimuthupravinraj@gmail.com>
2025-04-30 11:22:00 +0530

    Redo changes

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM yiisoftware/yii2-php:7.4-apache
-
-COPY . /boot/efi/php
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo
-echo "Deploying the latest updates to the container"
-docker stack deploy -c docker-compose.yaml webapp
-echo
-echo "Deployment is successfully completed."
-echo
